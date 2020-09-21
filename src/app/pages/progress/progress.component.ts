7891924e770f032a96dc9d8d7f15196af9360660
Diego Guaman <die1918@gmail.com>
2020-09-20 23:43:55 -0500

    pagina login y registro

---
+++ src/app/pages/progress/progress.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-progress',
+  templateUrl: './progress.component.html',
+  styles: [
+  ]
+})
+export class ProgressComponent implements OnInit {
+
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [
+  ]
+})
+export class SidebarComponent implements OnInit {
+
+numero22:string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
